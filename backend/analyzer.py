487367f20ad493f25490dfde7ec25d6dcdbf91d6
taecheon <kstkcy@naver.com>
2025-03-15 17:42:05 +0900

    top5 처음, 마지막 단어 반환

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -29,13 +29,15 @@ def get_numerical_data(data):
         'answerable_queries': 0,
         'unanswerable_queries': 0,
     }
-
     distributions = {
         'passage_length': {r: 0 for r in range(0, passage_length_term * 11, passage_length_term)},
         'query_length': {r: 0 for r in range(0, query_length_term * 11, query_length_term)},
         'answer_length': {r: 0 for r in range(0, answer_length_term * 11, answer_length_term)},
         'qp_precision': {r: 0 for r in range(0, qp_precision_term * 10, qp_precision_term)},
     }
+    first_phrases = defaultdict(int)
+    last_phrases = defaultdict(int)
+
 
     for e in data.data:
         sample_num['all_passages'] += len(e['paragraphs'])
@@ -50,7 +52,12 @@ def get_numerical_data(data):
             sample_num['all_queries'] += len(p['qas'])
 
             for qa in p['qas']:
-                query_length = len(qa['question'])
+                question = qa['question']
+
+                first_phrases[question.split()[0]] += 1
+                last_phrases[question.split()[-1]] += 1
+
+                query_length = len(question)
                 sample_value['avg_query_length'] += query_length
                 tmp_key = _get_distribution_key(query_length, query_length_term, query_length_term * 10)
                 distributions['query_length'][tmp_key] += 1
@@ -66,7 +73,7 @@ def get_numerical_data(data):
                 else:
                     sample_num['unanswerable_queries'] += 1
 
-                qp_precision, _, _ = get_f1_score(qa['question'], p['context'], 'phrase')
+                qp_precision, _, _ = get_f1_score(question, p['context'], 'phrase')
                 qp_precision *= 100
                 sample_value['avg_qp_precision'] += qp_precision
 
@@ -78,10 +85,16 @@ def get_numerical_data(data):
     sample_value['avg_answer_length'] = sample_value['avg_answer_length'] / sample_num['answerable_queries']
     sample_value['avg_qp_precision'] = sample_value['avg_qp_precision'] / sample_num['all_queries']
 
+    phrases = {
+        'first': first_phrases,
+        'last': last_phrases
+    }
+
     numerical_data = {
         'sample_num_info': sample_num,
         'sample_value_info': sample_value,
-        'distribution': distributions
+        'distribution': distributions,
+        'phrases': phrases
     }
 
     return numerical_data
